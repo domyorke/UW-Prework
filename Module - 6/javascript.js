9a25b06a2d89994a0f27ced5dc33bec939643d0d
Domyorke <dominicjenkins@hotmail.com>
2019-06-15 19:39:16 -0700

    Prework 6/15/19

---
+++ Module - 6/javascript.js
@@ -0,0 +1,15 @@
+function XXX() 
+{document.getElementById("box").style.backgroundColor = "Blue";}
+
+function YYY() 
+{document.getElementById("box").style.height = "300px";
+document.getElementById("box").style.width = "300px";}
+
+function ZZZ() 
+{document.getElementById("box").style.opacity = "0.5";}
+
+function ABC()
+{document.getElementById("box").style.backgroundColor = "Orange";
+document.getElementById("box").style.height = "150px";
+document.getElementById("box").style.width = "150px";
+document.getElementById("box").style.opacity = "1";}
